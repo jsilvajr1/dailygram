d874a5116d258d5bf9c926c6084b5a353b761793
Jorge Silva <jorge.silva.jr1@gmail.com>
2018-04-20 19:34:59 +0000

    heroku error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: dailygram-js.herokuapp.com}
+  config.action_mailer.default_url_options = { host: "dailygram-js.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
